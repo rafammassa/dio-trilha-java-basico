2fd53f0f34658c452b303c561816896bcbadf946
Rafaella Massa <rafaella_massa@hotmail.com>
2023-09-15 14:48:28 -0300

    Efetuando todas as entradas de dados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        int conta;
+        String agencia;
+        String nome;
+        float saldo;
+
+        System.out.println("Por favor, digite o número da conta:");
+        conta = scan.nextInt();
+
+        System.out.println("Agora, digite sua agência:");
+        agencia = scan.next();
+
+        System.out.println("Qual é o seu nome?");
+        nome = scan.next();
+
+        System.out.println("Por fim, digite seu saldo:");
+        saldo = scan.nextFloat();
+
     }
 }
